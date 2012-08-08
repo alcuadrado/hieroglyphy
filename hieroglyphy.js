73d3863152e21731fa8e14c17b454e9df39f0f94
Patricio Palladino <email@patriciopalladino.com>
2012-08-07 21:52:06 -0300

    Refactor for cleaner code and better API

--- hieroglyphy.js
+++ hieroglyphy.js
@@ -3,10 +3,19 @@
     "use strict";
 
     var numbers,
+        _object_Object,
+        _NaN,
+        _true,
+        _false,
+        _undefined,
+        _Infinity,
+        _1e100,
         characters,
         functionConstructor,
         escape,
-        unescape;
+        unescape,
+        locationString,
+        API;
 
     numbers = [
         "+[]",
@@ -22,57 +31,70 @@
     ];
 
     characters = {
-        "0" : "(" + numbers[0] + ")",
-        "1" : "(" + numbers[1] + ")",
-        "2" : "(" + numbers[2] + ")",
-        "3" : "(" + numbers[3] + ")",
-        "4" : "(" + numbers[4] + ")",
-        "5" : "(" + numbers[5] + ")",
-        "6" : "(" + numbers[6] + ")",
-        "7" : "(" + numbers[7] + ")",
-        "8" : "(" + numbers[8] + ")",
-        "9" : "(" + numbers[9] + ")",
-        " " : "({}+[])[!+[]+!![]+!![]+!![]+!![]+!![]+!![]]",
-        "[" : "({}+[])[+[]]",
-        "]" : "({}+[])[[]+ +!![]+(!+[]+!![]+!![]+!![])]",
-        "+" : "(+(+!![] + (!![]+[])[!+[]+!+[]+!+[]] + +!![] + +[] + +[])+[])[" +
-            "+[]+!![]+!![]]",
-        "a" : "(+{}+[])[+!![]]",
-        "b" : "({}+[])[!+[]+!![]]",
-        "c" : "({}+[])[!+[]+!+[]+!+[]+!+[]+!+[]]",
-        "d" : "([][+[]]+[])[!+[]+!![]]",
-        "e" : "(!![]+[])[!+[]+!![]+!![]]",
-        "f" : "([][+[]]+[])[!+[]+!![]+!![]+!![]]",
-        "i" : "([][+[]]+[])[!+[]+!![]+!![]+!![]+!![]]",
-        "j" : "({}+[])[!+[]+!![]+!![]]",
-        "l" : "(![]+[])[+[]+!![]+!![]]",
-        "n" : "([][+[]]+[])[+!![]]",
-        "o" : "({}+[])[+!![]]",
-        "r" : "(!![]+[])[+!![]]",
-        "s" : "(![]+[])[+[]+!![]+!![]+!![]]",
-        "t" : "(!![]+[])[+[]]",
-        "u" : "([][+[]]+[])[+[]]",
-        "y" : "(+(+!![] + (!![]+[])[!+[]+!+[]+!+[]] + +!![] + +[] + +[] + +[]" +
-            ")+[])[!+[]+!![]+!![]+!![]+!![]+!![]+!![]]",
-        "I" : "(+(+!![] + (!![]+[])[!+[]+!+[]+!+[]] + +!![] + +[] + +[] + +[]" +
-            ")+[])[+[]]",
-        "N" : "(+{}+[])[+[]]",
-        "O" : "({}+[])[!+[]+!![]+!![]+!![]+!![]+!![]+!![]+!![]]"
+        "0" : "(" + numbers[0] + "+[])",
+        "1" : "(" + numbers[1] + "+[])",
+        "2" : "(" + numbers[2] + "+[])",
+        "3" : "(" + numbers[3] + "+[])",
+        "4" : "(" + numbers[4] + "+[])",
+        "5" : "(" + numbers[5] + "+[])",
+        "6" : "(" + numbers[6] + "+[])",
+        "7" : "(" + numbers[7] + "+[])",
+        "8" : "(" + numbers[8] + "+[])",
+        "9" : "(" + numbers[9] + "+[])"
     };
+
+    _object_Object = "{}+[]";
+    _NaN = "+{}+[]";
+    _true = "!![]+[]";
+    _false = "![]+[]";
+    _undefined = "[][+[]]+[]";
+
+    characters[" "] = "(" + _object_Object + ")[" + numbers[7]  + "]";
+    characters["["] = "(" + _object_Object + ")[" + numbers[0]  + "]";
+    characters["]"] = "(" + _object_Object + ")[" + characters[1] + "+" +
+        characters[4] + "]";
+    characters["a"] = "(" + _NaN + ")[" + numbers[1] + "]";
+    characters["b"] = "(" + _object_Object + ")[" + numbers[2] + "]";
+    characters["c"] = "(" + _object_Object + ")[" + numbers[5] + "]";
+    characters["d"] = "(" + _undefined + ")[" + numbers[2] + "]";
+    characters["e"] = "(" + _undefined + ")[" + numbers[3] + "]";
+    characters["f"] = "(" + _undefined + ")[" + numbers[4] + "]";
+    characters["i"] = "(" + _undefined + ")[" + numbers[5] + "]";
+    characters["j"] = "(" + _object_Object + ")[" + numbers[3] + "]";
+    characters["l"] = "(" + _false + ")[" + numbers[2] + "]";
+    characters["n"] = "(" + _undefined + ")[" + numbers[1] + "]";
+    characters["o"] = "(" + _object_Object + ")[" + numbers[1] + "]";
+    characters["r"] = "(" + _true + ")[" + numbers[1] + "]";
+    characters["s"] = "(" + _false + ")[" + numbers[3] + "]";
+    characters["t"] = "(" + _true + ")[" + numbers[0] + "]";
+    characters["u"] = "(" + _undefined + ")[" + numbers[0] +"]";
+
+    characters["N"] = "(" + _NaN + ")[" + numbers[0] + "]";
+    characters["O"] = "(" + _object_Object + ")[" + numbers[8] + "]";
+
+    _Infinity = "+(" + numbers[1] + "+" + characters["e"] + "+" +
+        characters[1] + "+" + characters[0] + "+" + characters[0] + "+" +
+        characters[0] + ")+[]";
+
+    characters["y"] = "(" + _Infinity + ")[" + numbers[7] + "]";
+    characters["I"] = "(" + _Infinity + ")[" + numbers[0] + "]";
+
+    _1e100 = "+(" + numbers[1] + "+" + characters["e"] + "+" +
+        characters[1] + "+" + characters[0] + "+" + characters[0] + ")+[]";
+
+    characters["+"] = "(" + _1e100 + ")[" + numbers[2] + "]";
 
     functionConstructor = "[][" + hieroglyphyString("sort") + "][" +
         hieroglyphyString("constructor") + "]";
 
     //Below characters need target http(s) pages
-    characters["h"] = "([]+" + hieroglyphy("return location") + ")[" +
-        numbers[0] + "]";
-    characters["p"] = "([]+" + hieroglyphy("return location") + ")[" +
-        numbers[3] + "]";
-    characters["/"] = "([]+" + hieroglyphy("return location") + ")[" +
-        numbers[6] + "]";
+    locationString = "[]+" + hieroglyphyScript("return location");
+    characters["h"] = "(" + locationString + ")" + "[" + numbers[0] + "]";
+    characters["p"] = "(" + locationString + ")" + "[" + numbers[3] + "]";
+    characters["/"] = "(" + locationString + ")" + "[" + numbers[6] + "]";
 
-    unescape = hieroglyphy("return unescape");
-    escape = hieroglyphy("return escape");
+    unescape = hieroglyphyScript("return unescape");
+    escape = hieroglyphyScript("return escape");
 
     characters["%"] = escape + "(" + hieroglyphyString("[") + ")[" +
         numbers[0] + "]";
@@ -111,37 +133,24 @@
             return characters[char];
         }
 
-        if (char === "\\") {
-            //This char must be handled appart becuase the others need them
+        if ((char === "\\") || (char == "x")) {
+            //These chars must be handled appart becuase the others need them
             characters[char] = getUnescapeSequence(charCode);
             return characters[char];
         }
 
-        if (charCode < 128) {
-            //ASCII characters can be obtained with hexa and unscape sequences
-            unescapeSequence = getUnescapeSequence(charCode);
-
-            if (char !== "x") {
-                //Hexa sequences need the x.
-                hexaSequence = getHexaSequence(charCode);
-            }
-        }
-
         shortestSequence = getUnicodeSequence(charCode);
 
-        if (hexaSequence !== undefined) {
-            if (unescapeSequence.length < hexaSequence.length) {
-                if (shortestSequence.length > unescapeSequence.length) {
-                    shortestSequence = unescapeSequence;
-                }
-            } else {
-                if (shortestSequence.length > hexaSequence.length) {
-                    shortestSequence = hexaSequence;
-                }
-            }
-        } else if (unescapeSequence !== undefined) {
+        //ASCII characters can be obtained with hexa and unscape sequences
+        if (charCode < 128) {
+            unescapeSequence = getUnescapeSequence(charCode);
             if (shortestSequence.length > unescapeSequence.length) {
                 shortestSequence = unescapeSequence;
+            }
+
+            hexaSequence = getHexaSequence(charCode);
+            if (shortestSequence.length > hexaSequence.length) {
+                shortestSequence = hexaSequence;
             }
         }
 
@@ -162,16 +171,30 @@
         return hieroglyphiedStr;
     }
 
-    function hieroglyphy (src) {
+    function hieroglyphyNumber (n) {
+        if (n <= 9) {
+            return numbers[n];
+        }
+
+        return "+(" + hieroglyphyString(n.toString(10)) + ")";
+    }
+
+    function hieroglyphyScript (src) {
         return functionConstructor + "("  + hieroglyphyString(src) + ")()";
     }
 
+    API = {
+        hieroglyphyString: hieroglyphyString,
+        hieroglyphyNumber: hieroglyphyNumber,
+        hieroglyphyScript: hieroglyphyScript
+    };
+
     if (global.define && global.define.amd) {
-        global.define([], hieroglyphy);
+        global.define([], API);
     } else if (typeof exports !== "undefined") {
-        module.exports = hieroglyphy;
+        module.exports = API;
     } else {
-        global.hieroglyphy = hieroglyphy;
+        global.hieroglyphy = API;
     }
 
 })(this);
